56bd3e84ad014398cce5267498269640b4842e24
Paul Varache <perso@paulvarache.ninja>
2014-10-13 18:05:49 +0000

    Told mplayer to shut up with really quiet and now it's a lot better

--- node-ghettoblaster.js
+++ node-ghettoblaster.js
@@ -4,25 +4,27 @@ var cp = require('child_process'),
     spawn = cp.spawn,
     exec = cp.exec;
 
-function GBlatser(path){
+function GBlaster(path){
     this.childProc = null;
     this.file = path;
     this.playing = false;
     this.rl = null;
+    this.duration = null;
+    this.pausedPos = null;
 
     events.EventEmitter.call(this);
 
     exec('mplayer', function(err, stdout, stdin){
         if(err)
-            throw new Error("GBlatser encountered an error or isn't installed.");
+            throw new Error("GBlaster encountered an error or isn't installed.");
     });
 };
 
-GBlatser.prototype.__proto__ = events.EventEmitter.prototype;
+GBlaster.prototype.__proto__ = events.EventEmitter.prototype;
 
-GBlatser.prototype.play = function(opts) {
+GBlaster.prototype.play = function(opts, callback) {
     if (this.childProc !== null) return this.resume();             // The process is already running, we just resume the playing
-    var args = ['-slave', '-quiet', '-idle', this.file];
+    var args = ['-slave', '-quiet', '-idle', "-really-quiet", '-msglevel', 'statusline=6', '-msglevel', 'global=6', this.file];
 
     this.childProc = spawn('mplayer', args);
 
@@ -38,16 +40,9 @@ GBlatser.prototype.play = function(opts) {
         this.emit('error');
     }).bind(this));
 
-    var started = false;
-
     this.childProc.stdout.on('data', (function (data) {
-        if (data.toString().indexOf("Starting playback...") !== -1) {
-            started = true;
-            return;
-        }
-        if (data.toString() === "\n" && started) {
+        if (data.toString().indexOf("EOF") !== -1) {
             this.emit('ended');
-            started = false;
         }
     }).bind(this));
 
@@ -59,71 +54,103 @@ GBlatser.prototype.play = function(opts) {
         input: this.childProc.stdout,
         output: this.childProc.stdin
     });
+
     this.playing = true;
+    this.emit('started');
 };
 
-GBlatser.prototype.write = function (cmd) {
+GBlaster.prototype.write = function (cmd) {
     if(this.childProc !== null){
         this.childProc.stdin.write(cmd);
     }
 }
 
-GBlatser.prototype.stop = function() {
+GBlaster.prototype.stop = function() {
     if(this.childProc !== null){
         cp.exec("killall mplayer"); //Not really nice but works better than .kill('SIGINT')
         this.emit('stopped');
     }
 };
 
-GBlatser.prototype.toggle = function () {
+GBlaster.prototype.toggle = function () {
     this.write('pause\n');
     this.playing = !this.playing;
     this.emit(this.playing ? "resumed" : "paused");
 }
 
-GBlatser.prototype.pause = function() {
-    if (this.playing) this.toggle();
+GBlaster.prototype.pause = function() {
+    if (this.playing) {
+        this.getTimePosition((function (err, pos) {
+            console.log(pos);
+            this.lastPos = pos;
+            this.toggle();
+        }).bind(this));
+    }
 };
 
-GBlatser.prototype.resume = function () {
+GBlaster.prototype.resume = function () {
     if (!this.playing) this.toggle();
 }
 
-GBlatser.prototype.mute = function() {
+GBlaster.prototype.mute = function() {
     this.write('mute\n');
     this.emit('muted');
 };
 
-GBlatser.prototype.setVolume = function(volume) {
+GBlaster.prototype.setVolume = function(volume) {
     this.write('volume ' + volume + ' 1\n');
 };
 
-GBlatser.prototype.seek = function(sec) {
+GBlaster.prototype.seek = function(sec) {
     this.write('seek ' + sec + ' 2\n');
 };
 
-GBlatser.prototype.setLoop = function(times) {
+GBlaster.prototype.setLoop = function(times) {
     this.write('loop ' + times + '\n');
 };
 
-GBlatser.prototype.setSpeed = function(speed) {
+GBlaster.prototype.setSpeed = function(speed) {
     this.write('speed_set ' + speed + '\n');
 };
 
-GBlatser.prototype.getTimeLength = function(callback) {
-    if(this.childProc !== null){
-        this.rl.question("get_time_length\n", function(answer) {
-            callback(answer.split('=')[1]);
-        });
+GBlaster.prototype.getValue = function (value, callback) {
+    callback = callback || function () {}
+    if (this.childProc !== null) {
+        var question = this.playing ? "" : "pausing ";
+        question += "get_property ";
+        question += value;
+        this.rl.question(question + "\n", (function (answer) {
+            var s = answer.split("=");
+            if (s[1] && s[0] === "ANS_"+value) {
+                callback(null, s[1]);
+            } else {
+                setTimeout(this.getValue.bind(this, value, callback), 10); // Ugly hack to avoid shitty output from mplayer
+            }
+        }).bind(this));
     }
+}
+
+GBlaster.prototype.getPercentPos = function (callback) {
+    this.getValue("percent_pos", callback);
 };
 
-GBlatser.prototype.getTimePosition = function(callback) {
-    if(this.childProc !== null){
-        this.rl.question("get_time_pos\n", function(answer) {
-            callback(answer.split('=')[1]);
-        });
-    }
+GBlaster.prototype.getTimeLength = function(callback) {
+    if (this.duration !== null)
+        return callback(null, this.duration);
+    if (!this.playing)
+        return callback(null, 0);
+    this.getValue("length", (function (err, length) {
+        if (!err)
+            this.duration = length;
+        callback(err, length)
+    }).bind(this));
 };
 
-module.exports = GBlatser;
+GBlaster.prototype.getTimePosition = function(callback) {
+    if (!this.playing && this.pausedPos !== null) {
+        return callback(null, this.pausedPos);
+    }
+    this.getValue("time_pos", callback);
+};
+
+module.exports = GBlaster;
